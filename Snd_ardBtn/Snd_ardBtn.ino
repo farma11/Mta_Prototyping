9bb784cb087a86efa67d63d1d6575a5ae471b88e
Tatsuya Toyama <toyama.tatsuya@itolab.nitech.ac.jp>
2017-05-20 13:45:30 +0900

    複数スイッチ対応（音は同一）

--- Snd_ardBtn/Snd_ardBtn.ino
+++ Snd_ardBtn/Snd_ardBtn.ino
@@ -1,11 +1,26 @@
-const int button = 2; // ボタンのポート番号(Arduinoの2番ポート)
+// 各ボタンに対応するポート番号
+const int btn2 = 2;
+const int btn3 = 3;
+
+// ボタンの状態の確保
+int val = 0;
 
 void setup(){
   Serial.begin(9600); // PCへ送受信するための設定
+  pinMode(btn2, INPUT);
+  pinMode(btn3, INPUT);
 }
 
 void loop(){
-  Serial.write(digitalRead(button)); // PCへ送信
+  val = digitalRead(btn2);
+
   // シリアルにボタンの状態を書き出し（0が1になる）
+  if (val == HIGH){
+    Serial.write(val); // PCへ送信
+  } else {
+    val = digitalRead(btn3);
+    Serial.write(val);  // PCへ送信
+  }
+  
   delay(100); // 100msまつ
 }

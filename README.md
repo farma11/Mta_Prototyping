b8524beace0c7fb86d951d310959b7e0de91ee59
Tatsuya Toyama <toyama.tatsuya@itolab.nitech.ac.jp>
2017-05-20 15:46:14 +0900

    複数ボタン（ボタンごとの音対応）

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 ボタン一つで音を鳴らす
 ![1stCommit](./imgs/1stCommit.jpg "サンプル")
 
+参考: [Arduinoで実験　（タクトスイッチでブザー）](http://www.d-b.ne.jp/~k-kita/arduino_002.html)
+
 ### second Commit
 ボタン二つで音を鳴らす
 ![1stCommit](./imgs/2ndCommit.jpg "サンプル")
+
+参考: [【Arduino】タクトスイッチとシリアル通信](http://www.jonki.net/entry/20120116/1326722513)
--- Snd_ardBtn/Snd_ardBtn.ino
+++ Snd_ardBtn/Snd_ardBtn.ino
@@ -3,7 +3,7 @@ const int btn2 = 2;
 const int btn3 = 3;
 
 // ボタンの状態の確保
-int val = 0;
+// int val = 0;
 
 void setup(){
   Serial.begin(9600); // PCへ送受信するための設定
@@ -12,14 +12,12 @@ void setup(){
 }
 
 void loop(){
-  val = digitalRead(btn2);
 
   // シリアルにボタンの状態を書き出し（0が1になる）
-  if (val == HIGH){
-    Serial.write(val); // PCへ送信
-  } else {
-    val = digitalRead(btn3);
-    Serial.write(val);  // PCへ送信
+  if (digitalRead(btn2) == HIGH){
+    Serial.write(1); // PCへ送信
+  } else if(digitalRead(btn3) == HIGH){
+    Serial.write(2);  // PCへ送信
   }
   
   delay(100); // 100msまつ
